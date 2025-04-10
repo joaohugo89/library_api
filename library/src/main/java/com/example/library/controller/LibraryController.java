c353ce4c7cc044a93afc5d7abff24c348a4cd788
joaohugo89 <joaohugosoares89@outlook.com>
2025-04-10 14:06:28 -0300

    fix: update cors and poms updated

--- library/src/main/java/com/example/library/controller/LibraryController.java
+++ library/src/main/java/com/example/library/controller/LibraryController.java
@@ -3,9 +3,6 @@ package com.example.library.controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import org.springframework.web.bind.annotation.CrossOrigin;
-
-
 @RestController
 @RequestMapping("library")
 public class LibraryController {
